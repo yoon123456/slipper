1da454add5260d513c799f89b04a531bb52930af
joannenae <joannenaedevelop@gmail.com>
2022-05-16 10:19:18 +0900

    feature-#69_로고,회원가입

--- src/commons/styles/globalstyles.ts
+++ src/commons/styles/globalstyles.ts
@@ -5,11 +5,19 @@ export const globalStyles = css`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    /* font-family: "myfont"; */
+    font-size: 62.5%;
+    // font-size : 10px - 1rem
   }
 
   /* @font-face {
     font-family: "myfont";
     src: url("/font/scifibit.ttf");
   } */
+  @font-face {
+    font-family: "HallymGothic-Regular";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2204@1.0/HallymGothic-Regular.woff2")
+      format("woff2");
+    font-weight: 400;
+    font-style: normal;
+  }
 `;
--- src/component/unit/join/join.presenter.tsx
+++ src/component/unit/join/join.presenter.tsx
@@ -2,40 +2,44 @@ import * as S from "./join.styles";
 
 export default function JoinPresenter() {
   return (
-    <S.WrapperOut>
-      <S.WrapperIn>
-        <S.Join>Welcome!</S.Join>
+    <S.Wrapper>
+      <S.WrapperLeft>
+        <S.ImageBox>
+          <S.Image src="/image/check.png" />
+        </S.ImageBox>
+      </S.WrapperLeft>
+      <S.WrapperOut>
+        <S.WrapperIn>
+          <S.Join>Welcome to slipper!</S.Join>
 
-        <S.Profile>
-          <S.ProfileBox>
-            <S.InputProfile type="text" placeholder="아이디(이메일)" />
-            <S.InputProfile type="text" placeholder="닉네임" />
-          </S.ProfileBox>
-          <S.MyImage></S.MyImage>
-        </S.Profile>
+          <S.Profile>
+            <S.ProfileBox>
+              <S.InputProfile type="text" placeholder="아이디(이메일)" />
+              <S.InputProfile type="text" placeholder="닉네임" />
+            </S.ProfileBox>
+            <S.MyImage></S.MyImage>
+          </S.Profile>
 
-        <S.PasswordCheck type="password" placeholder="비밀번호" />
-        <S.PasswordCheck type="password" placeholder="비밀번호 확인" />
+          <S.PasswordCheck type="password" placeholder="비밀번호" />
+          <S.PasswordCheck type="password" placeholder="비밀번호 확인" />
 
-        <S.Check>
-          <S.TelBox>
-            <S.Tel type="tel" name="phone1" />-
-            <S.Tel type="tel" name="phone2" />-
-            <S.Tel type="tel" name="phone3" />
-          </S.TelBox>
+          <S.Check>
+            <S.TelBox>
+              <S.Tel type="tel" name="phone1" />-
+              <S.Tel type="tel" name="phone2" />-
+              <S.Tel type="tel" name="phone3" />
+            </S.TelBox>
 
-          <S.NumberAsk>인증번호 요청</S.NumberAsk>
-        </S.Check>
-        <S.CheckNumber>
-          <S.Input type="text" placeholder="인증번호" />
-          <S.CheckEnd>인증완료</S.CheckEnd>
-        </S.CheckNumber>
-
-        {/* <S.PasswordCheck type="password" placeholder="비밀번호" />
-        <S.PasswordCheck type="password" placeholder="비밀번호 확인" /> */}
-        <S.Introduce type="textarea" placeholder="간단한 소개글" />
-        <S.JoinSlipper>슬리퍼 신기</S.JoinSlipper>
-      </S.WrapperIn>
-    </S.WrapperOut>
+            <S.NumberAsk>인증번호 요청</S.NumberAsk>
+          </S.Check>
+          <S.CheckNumber>
+            <S.Input type="text" placeholder="인증번호" />
+            <S.CheckEnd>인증완료</S.CheckEnd>
+          </S.CheckNumber>
+          <S.Introduce type="textarea" placeholder="간단한 소개글" />
+          <S.JoinSlipper>슬리퍼 신기</S.JoinSlipper>
+        </S.WrapperIn>
+      </S.WrapperOut>
+    </S.Wrapper>
   );
 }
