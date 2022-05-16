b7f8ad111dd2c1351e69f09f11bf0ee7a38a41f1
joannenae <joannenaedevelop@gmail.com>
2022-05-16 18:16:52 +0900

    feature#69_settimeout

--- src/component/unit/join/join.styles.ts
+++ src/component/unit/join/join.styles.ts
@@ -10,11 +10,13 @@ export const ImageBox = styled.div`
   margin-top: 40px;
 `;
 export const Image = styled.img`
-  width: 100%;
+  width: 90%;
+  margin-left: 10%;
 `;
 export const WrapperLeft = styled.div`
   width: 40%;
   background-color: white;
+  /* border-right: 1px solid gainsboro; */
 `;
 
 export const WrapperOut = styled.div`
@@ -25,44 +27,37 @@ export const WrapperOut = styled.div`
   background-color: white;
 `;
 export const WrapperIn = styled.div`
-  width: 60%;
+  width: 40%;
   margin: 0 auto;
   padding-top: 40px;
 `;
 export const Join = styled.div`
   text-align: center;
-  font-size: 50px;
+  font-size: 5rem;
   font-weight: bold;
   color: #008af6;
-  margin-top: 50px;
 `;
 export const Profile = styled.div`
   display: flex;
   justify-content: space-between;
-
-  padding-top: 70px;
+  padding-top: 60px;
 `;
 export const ProfileBox = styled.div`
   width: 70%;
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 `;
 export const InputProfile = styled.input`
-  width: 100%;
+  width: 70%;
   border: none;
   border-bottom: 2px solid gainsboro;
   padding: 10px;
   /* margin: 10px; */
 `;
-export const Input = styled.input`
-  width: 20%;
-  border: 1px solid gainsboro;
-  margin-right: 20px;
-  border-radius: 5px;
-`;
 export const MyImage = styled.div`
-  width: 120px;
-  height: 120px;
+  width: 150px;
+  height: 150px;
   background-color: gainsboro;
 `;
 export const Check = styled.div`
@@ -75,32 +70,43 @@ export const CheckNumber = styled.div`
 `;
 
 export const TelBox = styled.div`
-  width: 60%;
-  height: 20%;
+  width: 35%;
 `;
 export const Tel = styled.input`
   width: 30%;
+  height: 25px;
   border: none;
   border: 1px solid gainsboro;
 `;
 export const NumberAsk = styled.div`
   width: 100px;
+  line-height: 3;
   text-align: center;
   background-color: #f2f2f2;
   border-radius: 10px;
 `;
+
+export const Input = styled.input`
+  width: 33%;
+  height: 30px;
+  border: 1px solid gainsboro;
+  margin-right: 15px;
+  border-radius: 5px;
+`;
 export const CheckEnd = styled.div`
   width: 100px;
+  line-height: 3;
   text-align: center;
   background-color: #f2f2f2;
   border-radius: 10px;
 `;
 export const PasswordCheck = styled.input`
-  width: 40%;
+  width: 48%;
   display: block;
   border: none;
   border-bottom: 2px solid gainsboro;
   padding: 10px;
+  margin-top: 40px;
 `;
 export const Introduce = styled.input`
   width: 100%;
