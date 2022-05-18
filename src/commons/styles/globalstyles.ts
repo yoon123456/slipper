73123eb2d490bfda44bcd7b7491ed4a100ca6d7d
joannenae <joannenaedevelop@gmail.com>
2022-05-18 18:23:46 +0900

    feature#102-폰트수정

--- src/commons/styles/globalstyles.ts
+++ src/commons/styles/globalstyles.ts
@@ -30,12 +30,15 @@ export const globalStyles = css`
     font-weight: 400;
     font-style: normal;
   }
-
+  /* latin */
   @font-face {
     font-family: "Luckiest Guy";
-    src: url("https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap%27")
-      /* src: local("Luckiest Guy") url("/fonts/LuckiesGuy-Regular.woff") */
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/luckiestguy/v18/_gP_1RrxsjcxVyin9l9n_j2hTd52ijl7aQ.woff2)
       format("woff2");
-    font-display: swap;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+      U+2215, U+FEFF, U+FFFD;
   }
 `;
