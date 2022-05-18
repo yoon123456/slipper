9478831520d4c1afd40cbcc22b5afe319c82ae4e
yoon123456 <yw01124@gmail.com>
2022-05-18 10:08:06 +0900

    feature-#79 상세페이지

--- src/component/unit/boards/detail/detail.presenter.tsx
+++ src/component/unit/boards/detail/detail.presenter.tsx
@@ -2,42 +2,82 @@
 
 import Category from "../../../../commons/category";
 import * as S from "./detail.styles";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function DetailPresenter() {
+  const settings = {
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 4000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+  };
   return (
     <S.WrapperOut>
       <S.WrapperTop>
         <S.TopLeft>
-          <S.UserTop>사진</S.UserTop>
-          <S.UserMiddle>
-            <S.UserName>유저닉네임</S.UserName>
-          </S.UserMiddle>
-          <S.UserBottom>
-            <S.UserContents>
-              안녕하세요 슬리퍼 신으러
-              왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
-            </S.UserContents>
-          </S.UserBottom>
+          <S.Map src={"/image/mapEx.png"} />
+          <S.SliderWrap>
+            <S.ImageWrap>
+              <S.SliderStyle {...settings}>
+                <S.ImgWrapper>
+                  <S.Img src={"/image/kakao.png"} />
+                </S.ImgWrapper>
+                <S.ImgWrapper>
+                  <S.Img src={"/image/test2.png"} />
+                </S.ImgWrapper>
+                <S.ImgWrapper>
+                  <S.Img src={"/image/naver.png"} />
+                </S.ImgWrapper>
+                <S.ImgWrapper>
+                  <S.Img src={"/image/mypicks.png"} />
+                </S.ImgWrapper>
+              </S.SliderStyle>
+            </S.ImageWrap>
+          </S.SliderWrap>
         </S.TopLeft>
         <S.TopRight>
-          <S.Map src={"/image/mapEx.png"} />
+          <S.UserInfoWrap>
+            <S.UserImg src={"/image/usericon.png"} />
+            <S.UserWrap>
+              <S.UserName>작성자</S.UserName>
+              <S.UserWriteDate>작성날짜</S.UserWriteDate>
+            </S.UserWrap>
+          </S.UserInfoWrap>
+          <S.UserTitle>사장님이 친절해요</S.UserTitle>
+          <S.UserLiving>2022.05.07~2222.05.07</S.UserLiving>
           <S.CategoryWrap>
             <Category />
           </S.CategoryWrap>
+          <S.UserMiddle>
+            <S.ShopName>코딩맛집</S.ShopName>
+            <S.ShopRatingWrap>
+              <S.ShopRating>만족도</S.ShopRating>
+              <S.RatingWrap>
+                <S.Good src={"/image/ratingGood.png"} />
+                <S.Soso src={"/image/ratingSoso.png"} />
+                <S.Bad src={"/image/ratingBad.png"} />
+              </S.RatingWrap>
+            </S.ShopRatingWrap>
+          </S.UserMiddle>
+          <S.UserContents>
+            안녕하세요 슬리퍼 신으러
+            왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
+            안녕하세요 슬리퍼 신으러
+            왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
+            안녕하세요 슬리퍼 신으러
+            왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
+            안녕하세요 슬리퍼 신으러
+            왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
+            안녕하세요 슬리퍼 신으러
+            왔어요오오옹오오오오오오오옹오오오오오오오오옹오오오오오오옹오오오오오오오
+            안녕하세요 슬리퍼 신으러
+          </S.UserContents>
         </S.TopRight>
       </S.WrapperTop>
-      <S.WrapperMiddle>
-        <S.Title>우리동네 맛집은 나만 알거야</S.Title>
-        <S.ResidenceWrap>
-          <S.Residence>거주기간</S.Residence>
-          <S.Rating>
-            <S.Good src={"/image/ratingGood.png"} />
-            <S.Soso src={"/image/ratingSoso.png"} />
-            <S.Bad src={"/image/ratingBad.png"} />
-          </S.Rating>
-        </S.ResidenceWrap>
-        <S.Contents>우리동네에는 맛집이 많아요 수원으로 놀러 오세요</S.Contents>
-      </S.WrapperMiddle>
       <S.WrapperBottom>
         <S.Button>목록</S.Button>
         <S.Button>수정</S.Button>
--- src/component/unit/boards/detail/detail.styles.tsx
+++ src/component/unit/boards/detail/detail.styles.tsx
@@ -1,91 +1,140 @@
 import styled from "@emotion/styled";
+import Slider from "react-slick";
 
 export const WrapperOut = styled.div`
   width: 50%;
-  margin: 0 auto;
-  padding-top: 40px;
+  height: 650px;
+  margin: 3% auto;
+  padding: 1%;
   display: flex;
   flex-direction: column;
+  box-shadow: 2px 2px 4px 4px #e1e1e1;
 `;
 export const WrapperTop = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
 `;
 export const TopLeft = styled.div`
-  width: 30%;
-`;
-export const UserTop = styled.div`
-  width: 100%;
-  height: 180px;
-  background-color: aliceblue;
-`;
-export const UserMiddle = styled.div`
+  width: 50%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
 `;
-export const UserName = styled.div`
-  font-size: 16px;
-  padding: 5px;
-  color: white;
-  background-color: #008af6;
-  border-radius: 30px;
+// slider
+export const SliderWrap = styled.div`
+  width: 90%;
+  margin: 4% auto 0 auto;
+`;
+export const ImageWrap = styled.div`
+  width: 100%;
+  height: 150px;
 `;
 
-export const UserBottom = styled.div`
-  height: 50px;
+export const SliderStyle = styled(Slider)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ImgWrapper = styled(Slider)`
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
-  background-color: bisque;
+  margin: 0 auto;
+  width: 100%;
+  height: 30%;
 `;
-export const UserContents = styled.div`
-  font-size: 15px;
+export const Img = styled.img`
+  width: 40%;
+  height: 150px;
+  object-fit: cover;
+  background-color: green;
+  margin-right: 10px;
+  border-radius: 5%;
+`;
+//
+export const TopRight = styled.div`
+  width: 50%;
+  padding: 2%;
+`;
+export const UserInfoWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
   align-items: center;
 `;
-export const TopRight = styled.div`
-  width: 80%;
-`;
-export const Map = styled.img`
-  width: 100%;
-  height: 200px;
-`;
-export const CategoryWrap = styled.div`
-  width: 100%;
-`;
-export const WrapperMiddle = styled.div`
-  width: 100%;
+export const UserWrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding-top: 20px;
 `;
-export const Title = styled.div`
+export const UserImg = styled.img``;
+export const UserName = styled.div`
+  font-size: 1.6rem;
+  padding: 5px;
+`;
+export const UserWriteDate = styled.div``;
+
+export const UserTitle = styled.div`
+  width: 100%;
+  height: 50px;
+  font-size: 2.5rem;
+`;
+export const UserLiving = styled.div`
   width: 100%;
   height: 30px;
-  padding-left: 5%;
-  border-bottom: 1px solid #d8d8d8;
-  font-size: 20px;
-  margin-bottom: 10px;
+  font-size: 1.5rem;
 `;
-export const ResidenceWrap = styled.div`
+export const UserMiddle = styled.div`
   width: 100%;
-  height: 30px;
+  height: 50px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2%;
 `;
-export const Residence = styled.div`
-  width: 80%;
-  padding-left: 5%;
-  border-bottom: 1px solid #d8d8d8;
-  font-size: 16px;
+export const ShopName = styled.div`
+  width: 30%;
+  font-size: 1.6rem;
 `;
-export const Rating = styled.div`
-  width: 12%;
-  font-size: 16px;
+export const ShopRatingWrap = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
+export const ShopRating = styled.div`
+  width: 20%;
+  font-size: 1.6rem;
+`;
+export const RatingWrap = styled.div`
+  width: 30%;
+`;
+
+export const UserContents = styled.div`
+  width: 95%;
+  height: 250px;
+  font-size: 1.5rem;
+  padding: 2%;
+  margin-top: 2%;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  /* border: 1px solid gainsboro; */
+  border-radius: 3%;
+  background-color: #fbfbfd;
+`;
+
+export const Map = styled.img`
+  width: 100%;
+  height: 300px;
+`;
+export const CategoryWrap = styled.div`
+  width: 100%;
+`;
+
 export const Good = styled.img`
   width: 33.3%;
 `;
@@ -95,10 +144,7 @@ export const Soso = styled.img`
 export const Bad = styled.img`
   width: 30%;
 `;
-export const Contents = styled.div`
-  width: 100%;
-  height: 200px;
-`;
+
 export const WrapperBottom = styled.div`
   width: 50%;
   margin: 0 auto;
