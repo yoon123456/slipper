655c54c2fb32404e0d13b9212a61d7176ad3087d
yoon123456 <yw01124@gmail.com>
2022-05-25 15:37:01 +0900

    feature-#182 수정

--- src/commons/searchBarCaterory/searchBarCategory.types.ts
+++ src/commons/searchBarCaterory/searchBarCategory.types.ts
@@ -4,6 +4,7 @@ import { RecoilState } from "recoil";
 export interface ISearchBarCategoryPresenter {
   isActive: boolean;
   category: RecoilState<string>;
+  search: string;
   onMouseDown: () => void;
   onChangeSearch: (event: ChangeEvent<HTMLInputElement>) => void;
   onClickCategory: (event: any) => void;
