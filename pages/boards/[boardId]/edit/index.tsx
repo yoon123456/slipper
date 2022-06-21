b88ac0752f875f387627dd9e15e953a8919766b1
joannenae <joannenaedevelop@gmail.com>
2022-06-21 13:18:40 +0900

    회원가입 수정

--- pages/boards/[boardId]/edit/index.tsx
+++ pages/boards/[boardId]/edit/index.tsx
@@ -9,34 +9,7 @@ import WriteContainer from "../../../../src/component/unit/boards/write/write.co
 
 const FETCH_BOARD = gql`
   query fetchBoard($boardId: String!) {
-    fetchBoard(boardId: $boardId) {
-      id
-      createdAt
-      nickname
-      category
-      title
-      contents
-      thumbnail
-      score
-      startDate
-      endDate
-      lat
-      lng
-      address
-      place
-      placePhone
-      placeUrl
-      likeCount
-      images {
-        id
-        imageUrl
-      }
-      user {
-        id
-        imageUrl
-        role
-      }
-    }
+    fetchBoard(boardId: $boardId)
   }
 `;
 
